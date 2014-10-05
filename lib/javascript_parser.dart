3a220801219f62ae91ac44a18c73b5c0a74f12b6
asgerf <asgerklasker@gmail.com>
2014-10-05 21:12:14 +0200

    ..

--- lib/javascript_parser.dart
+++ lib/javascript_parser.dart
@@ -2,16 +2,16 @@ library javascript_parser;
 
 import 'dart:io';
 import 'dart:async';
-import 'ast.dart';
-import 'lexer.dart';
-import 'parser.dart';
 
-export 'ast.dart';
-export 'lexer.dart' show ParseError;
+import 'src/ast.dart';
+import 'src/lexer.dart';
+import 'src/parser.dart';
+
+export 'src/ast.dart';
+export 'src/lexer.dart' show ParseError;
 
 Program parse(String text, {String filename, int firstLine : 1}) {
-  Lexer lexer = new Lexer(text, filename: filename, currentLine: firstLine);
-  return new Parser(lexer).parseProgram();
+  return new Parser(new Lexer(text, filename: filename, currentLine: firstLine)).parseProgram();
 }
 
 Future<Program> parseFile(File file) => file.readAsString().then((String text) => parse(text, filename: file.path));
--- test/test_lexer.dart
+++ test/test_lexer.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-import '../bin/lexer.dart';
+import '../lib/src/lexer.dart';
 
 void main() {
   
